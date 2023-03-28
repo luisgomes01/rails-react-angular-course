40fec24c0d2eadf6df8faf359c4bb6237a494684
Luís Gomes <luis252001@hotmail.com>
2023-03-28 17:09:08 -0300

    How to create view files in Angular

--- freelance-camp-fe/src/app/app.component.ts
+++ freelance-camp-fe/src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: `
-  <h1>App file - Navigation</h1>
-  <router-outlet></router-outlet>
-  `,
+  templateUrl: 'app.component.html' ,
 })
 
 export class AppComponent {
--- freelance-camp-fe/src/app/homepage/homepage.component.ts
+++ freelance-camp-fe/src/app/homepage/homepage.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'homepage',
-    template: '<h1>Homepage Component</h1>'
+    templateUrl: 'homepage.component.html'
 })
 
 export class HomepageComponent {}
