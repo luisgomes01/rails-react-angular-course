711022b1f119067026d898c4ee810d26a80afd17
Luís Gomes <luis252001@hotmail.com>
2023-03-28 17:26:52 -0300

    Creating Links Between Pages in Angular

--- freelance-camp-fe/src/app/app.module.ts
+++ freelance-camp-fe/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { DocumentsComponent } from './documents/documents.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [BrowserModule, AppRoutingModule],
-  declarations: [AppComponent, HomepageComponent],
+  declarations: [AppComponent, HomepageComponent, DocumentsComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ freelance-camp-fe/src/app/documents/documents.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'document',
+    templateUrl: 'documents.component.html',
+})
+
+export class DocumentsComponent {}
