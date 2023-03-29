11ea87a0f457f6e13da8d5429465997741eaf1d0
Luís Gomes <luis252001@hotmail.com>
2023-03-28 21:42:43 -0300

    Creating Mock Data in an Angular 2 Application

--- freelance-camp-fe/src/app/documents/documents.component.ts
+++ freelance-camp-fe/src/app/documents/documents.component.ts
@@ -1,8 +1,32 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
-    selector: 'document',
-    templateUrl: 'documents.component.html',
+  selector: 'document',
+  templateUrl: 'documents.component.html',
 })
-
-export class DocumentsComponent {}
+export class DocumentsComponent {
+  documents: Document[] = [
+    {
+      title: 'First Document',
+      description: 'First Doc description',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com',
+    },
+    {
+      title: 'Second Document',
+      description: 'Second Doc description',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com',
+    },
+    {
+      title: 'Third Document',
+      description: 'Third Doc description',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com',
+    },
+  ];
+}
