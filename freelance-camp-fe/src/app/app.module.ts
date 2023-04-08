0472d6cc652bd8673654917a25b275abdedb7d24
Luís Gomes <luis252001@hotmail.com>
2023-04-08 11:26:18 -0300

    Creating an Angular 2 Service for Managing API Data

--- freelance-camp-fe/src/app/app.module.ts
+++ freelance-camp-fe/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { DocumentService } from './documents/document.service';
 
-import { AppRoutingModule } from './app-routing.module';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
 import { ProposalShowComponent } from './proposal/proposal-show.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [BrowserModule, AppRoutingModule, FormsModule, NgbModule],
@@ -21,6 +23,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     ProposalListComponent,
     ProposalNewComponent,
     ProposalShowComponent
+  ],
+  providers: [
+    DocumentService
   ],
   bootstrap: [AppComponent],
 })
