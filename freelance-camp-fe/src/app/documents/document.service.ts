f3cabbfd25246eeff68f0194c7504abb6ab11d74
Luís Gomes <luis252001@hotmail.com>
2023-04-11 19:53:10 -0300

    Create service for documents

--- freelance-camp-fe/src/app/documents/document.service.ts
+++ freelance-camp-fe/src/app/documents/document.service.ts
@@ -1,7 +1,33 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+import { Document } from './document';
 
 @Injectable()
-
 export class DocumentService {
-    
+  private documentsUrl = 'http://localhost:3001/freelance_documents.json';
+
+  constructor(private http: HttpClient) {}
+
+  getDocuments(): Observable<Document[]> {
+    return this.http.get<Document[]>(this.documentsUrl).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
 }
