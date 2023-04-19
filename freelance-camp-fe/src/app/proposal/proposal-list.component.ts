60bc5c2adf8db8f6c1f0e75646856f1866ad0d4e
Luís Gomes <luis252001@hotmail.com>
2023-04-18 22:40:23 -0300

    Create method to go to show selected proposal page

--- freelance-camp-fe/src/app/proposal/proposal-list.component.ts
+++ freelance-camp-fe/src/app/proposal/proposal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Proposal } from './proposal';
 import { ProposalService } from './proposal.service';
 import { timer } from 'rxjs';
@@ -11,7 +12,10 @@ export class ProposalListComponent implements OnInit {
   proposals: Proposal[] = [];
   errorMessage: string;
 
-  constructor(private proposalService: ProposalService) {}
+  constructor(
+    private proposalService: ProposalService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     const interval = timer(0, 5000);
@@ -23,5 +27,10 @@ export class ProposalListComponent implements OnInit {
       next: (proposals) => (this.proposals = proposals),
       error: (error) => (this.errorMessage = <any>error),
     });
+  }
+
+  goToShow(proposal: Proposal): void {
+    let link = ['/proposal', proposal.id]
+    this.router.navigate(link)
   }
 }
