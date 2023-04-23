75474fdfd389d8cfa2195b0fa9eebabeec02b462
Luís Gomes <luis252001@hotmail.com>
2023-04-23 17:39:56 -0300

    Configure rails api session store to work in all environments

--- authentication_app/config/initializers/session_store.rb
+++ authentication_app/config/initializers/session_store.rb
@@ -1,1 +1,6 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "http://localhost:3000"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "http://localhost:3000"
+
+else
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
