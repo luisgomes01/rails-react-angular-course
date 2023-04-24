0d84b575dab284cb45cf97ab74c39dc55250c29c
Luís Gomes <luis252001@hotmail.com>
2023-04-24 17:09:50 -0300

    create dashboard and home routes

---
+++ ReactAuthApp/src/components/Dashboard.tsx
@@ -0,0 +1,11 @@
+
+export default function Dasbhoard () {
+    return (
+        <nav>
+            <div>
+                <h1>Dashboard</h1>
+                <h1>Dashboard</h1>
+            </div>
+        </nav>
+    )
+}
